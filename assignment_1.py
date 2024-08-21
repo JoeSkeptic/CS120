857e7ffa724d2fca22a46dd06f38f86903d3afe0
Joe Skeptic <Skeptical1968@gmail.com>
2024-08-21 14:52:09 -0600

    created assignment 1, to print 3 friends names

---
+++ assignment_1.py
@@ -0,0 +1,8 @@
+""" Prints name of three friends, with new lines between """
+#Sam Hodges, CS120, Assignment 1
+
+NAME1 = "Joe"
+NAME2 = "Mark"
+NAME3 = "Sally"
+
+print(NAME1,"\n",NAME2,"\n",NAME3)
